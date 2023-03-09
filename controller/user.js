1f6066e398eeece768afbc0f7b5bc50825cec585
UTHAYASANMUGAM.P <91073194+uthayasanmugam@users.noreply.github.com>
2023-03-09 15:35:57 +0530

    Add files via upload

---
+++ controller/user.js
@@ -0,0 +1,41 @@
+import User from "../model/user.js"
+
+export const userRegister = async (req, res) => {    
+    let email = req.body.email
+    let exUser = await User.findOne({ email: email })
+    if (exUser) {
+        return res.status(400).json({ message: "email already register" })
+    }
+    else {
+        let register = new User({
+            vendorname : req.body. vendorname,
+            vendortype: req.body.vendortype,
+            address: req.body.address,
+            pincode:req.body.pincode,
+            email: req.body.email,
+            mobileno: req.body.mobileno,
+            
+          //image: req.body.gender,    
+            })
+            try {
+                let user=await register.save()
+                res.status(201).json({ message: "Register success" ,id:user._id})
+            } catch (error) {
+                res.status(400).json({ message: error.message });
+            }
+     }
+}
+export const getsingleUser = async (req, res) => {
+    try {
+        const getUser = await User.find({ address: req.query.address}).select("-password")
+        res.status(200).json({ data: getUser })
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+      }
+
+//         res.sent(200).json({ data: getUser })
+//     } catch (error) {
+//         res.status(400).json({ message: error.message });
+//     }
+// }
